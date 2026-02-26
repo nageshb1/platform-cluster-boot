fba340a786edc75acac698656954a889ab7cae97
Nagesh Bhuvanagiri <nag.primary@gmail.com>
2026-02-26 16:54:18 -0700

    commented the output variables

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -19,6 +19,7 @@ output "public_subnet_ids" {
   value       = module.vpc.public_subnets
 }
 
+/*
 # EKS Outputs
 output "cluster_name" {
   description = "Name of the EKS cluster"
@@ -34,3 +35,4 @@ output "cluster_security_group_id" {
   description = "Security group ID attached to the EKS cluster"
   value       = module.eks.cluster_security_group_id
 }
+*/
